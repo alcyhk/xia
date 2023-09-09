8313d989fbb805e695895dc2e38b211e54000c4b
alcyhk <alcyhk@users.noreply.github.com>
2023-09-09 20:30:49 +0800

    Add files via upload

---
+++ hdk/arty35/skycdc.gen/sources_1/ip/multiplier/multiplier_stub.vhdl
@@ -0,0 +1,33 @@
+-- Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2021.2 (lin64) Build 3367213 Tue Oct 19 02:47:39 MDT 2021
+-- Date        : Tue Nov 15 09:26:04 2022
+-- Host        : computer running 64-bit Ubuntu 22.04.1 LTS
+-- Command     : write_vhdl -force -mode synth_stub -rename_top multiplier -prefix
+--               multiplier_ multiplier_stub.vhdl
+-- Design      : multiplier
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a35ticsg324-1L
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity multiplier is
+  Port ( 
+    CLK : in STD_LOGIC;
+    A : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    B : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    P : out STD_LOGIC_VECTOR ( 31 downto 0 )
+  );
+
+end multiplier;
+
+architecture stub of multiplier is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "CLK,A[31:0],B[31:0],P[31:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "mult_gen_v12_0_17,Vivado 2021.2";
+begin
+end;
