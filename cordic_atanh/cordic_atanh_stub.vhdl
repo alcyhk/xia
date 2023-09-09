7d1ee8ef4e4bd414c60dea06a2139e6dbb7abfd6
alcyhk <alcyhk@users.noreply.github.com>
2023-09-09 19:56:26 +0800

    Add files via upload

---
+++ cordic_atanh/cordic_atanh_stub.vhdl
@@ -0,0 +1,35 @@
+-- Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2021.2 (lin64) Build 3367213 Tue Oct 19 02:47:39 MDT 2021
+-- Date        : Tue Nov 15 10:27:57 2022
+-- Host        : computer running 64-bit Ubuntu 22.04.1 LTS
+-- Command     : write_vhdl -force -mode synth_stub
+--               /home/albert/work/vivado/arty35/skycdc.gen/sources_1/ip/cordic_atanh/cordic_atanh_stub.vhdl
+-- Design      : cordic_atanh
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a35ticsg324-1L
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity cordic_atanh is
+  Port ( 
+    aclk : in STD_LOGIC;
+    s_axis_cartesian_tvalid : in STD_LOGIC;
+    s_axis_cartesian_tready : out STD_LOGIC;
+    s_axis_cartesian_tdata : in STD_LOGIC_VECTOR ( 63 downto 0 );
+    m_axis_dout_tvalid : out STD_LOGIC;
+    m_axis_dout_tdata : out STD_LOGIC_VECTOR ( 31 downto 0 )
+  );
+
+end cordic_atanh;
+
+architecture stub of cordic_atanh is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "aclk,s_axis_cartesian_tvalid,s_axis_cartesian_tready,s_axis_cartesian_tdata[63:0],m_axis_dout_tvalid,m_axis_dout_tdata[31:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "cordic_v6_0_17,Vivado 2021.2";
+begin
+end;
---
+++ cordic_rotate/cordic_rotate_stub.v
@@ -0,0 +1,30 @@
+// Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2021.2 (lin64) Build 3367213 Tue Oct 19 02:47:39 MDT 2021
+// Date        : Tue Nov 15 10:16:55 2022
+// Host        : computer running 64-bit Ubuntu 22.04.1 LTS
+// Command     : write_verilog -force -mode synth_stub
+//               /home/albert/work/vivado/arty35/skycdc.gen/sources_1/ip/cordic_rotate/cordic_rotate_stub.v
+// Design      : cordic_rotate
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7a35ticsg324-1L
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "cordic_v6_0_17,Vivado 2021.2" *)
+module cordic_rotate(aclk, s_axis_phase_tvalid, 
+  s_axis_phase_tready, s_axis_phase_tdata, s_axis_cartesian_tvalid, 
+  s_axis_cartesian_tready, s_axis_cartesian_tdata, m_axis_dout_tvalid, m_axis_dout_tdata)
+/* synthesis syn_black_box black_box_pad_pin="aclk,s_axis_phase_tvalid,s_axis_phase_tready,s_axis_phase_tdata[31:0],s_axis_cartesian_tvalid,s_axis_cartesian_tready,s_axis_cartesian_tdata[63:0],m_axis_dout_tvalid,m_axis_dout_tdata[63:0]" */;
+  input aclk;
+  input s_axis_phase_tvalid;
+  output s_axis_phase_tready;
+  input [31:0]s_axis_phase_tdata;
+  input s_axis_cartesian_tvalid;
+  output s_axis_cartesian_tready;
+  input [63:0]s_axis_cartesian_tdata;
+  output m_axis_dout_tvalid;
+  output [63:0]m_axis_dout_tdata;
+endmodule
