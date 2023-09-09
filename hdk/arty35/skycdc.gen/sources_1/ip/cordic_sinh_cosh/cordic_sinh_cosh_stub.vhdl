173285afeb3c7b2015cc5c5992803e1d63ea4606
alcyhk <alcyhk@users.noreply.github.com>
2023-09-09 20:03:19 +0800

    Add files via upload

---
+++ hdk/arty35/skycdc.gen/sources_1/ip/cordic_sinh_cosh/cordic_sinh_cosh_stub.vhdl
@@ -0,0 +1,35 @@
+-- Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2021.2 (lin64) Build 3367213 Tue Oct 19 02:47:39 MDT 2021
+-- Date        : Tue Nov 15 12:42:57 2022
+-- Host        : computer running 64-bit Ubuntu 22.04.1 LTS
+-- Command     : write_vhdl -force -mode synth_stub
+--               /home/albert/work/vivado/arty35/skycdc.gen/sources_1/ip/cordic_sinh_cosh/cordic_sinh_cosh_stub.vhdl
+-- Design      : cordic_sinh_cosh
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a35ticsg324-1L
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity cordic_sinh_cosh is
+  Port ( 
+    aclk : in STD_LOGIC;
+    s_axis_phase_tvalid : in STD_LOGIC;
+    s_axis_phase_tready : out STD_LOGIC;
+    s_axis_phase_tdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    m_axis_dout_tvalid : out STD_LOGIC;
+    m_axis_dout_tdata : out STD_LOGIC_VECTOR ( 63 downto 0 )
+  );
+
+end cordic_sinh_cosh;
+
+architecture stub of cordic_sinh_cosh is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "aclk,s_axis_phase_tvalid,s_axis_phase_tready,s_axis_phase_tdata[31:0],m_axis_dout_tvalid,m_axis_dout_tdata[63:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "cordic_v6_0_17,Vivado 2021.2";
+begin
+end;
